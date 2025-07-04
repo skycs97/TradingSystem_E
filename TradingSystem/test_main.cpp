fe0f576db46f8578807a9ff7462d8278630c7216
skycs97 <skycs97@gmail.com>
2025-07-04 09:36:49 +0900

    add unitest for login

--- TradingSystem/test_main.cpp
+++ TradingSystem/test_main.cpp
@@ -1,6 +1,68 @@
 #include <gmock/gmock.h>
-#include "kiwer.cpp""
-#include "nemo_api.cpp"
+#include <string>
+#include "kiwer.h""
+#include "nemo_api.h"
+#include "driver.h"
+
+using std::string;
+
+class MockDriver : public Driver {
+public:
+	MOCK_METHOD(void, loginSystem, (string, string), (override));
+	MOCK_METHOD(void, buyStock, (string, int, int), (override));
+	MOCK_METHOD(void, sellStock, (string, int, int), (override));
+	MOCK_METHOD(int, getMarketPrice, (string, int), (override));
+};
+
+TEST(TradingSystem, NemoLogin) {
+	NemoAPI *nemoApi = new NemoAPI();
+	KiwerAPI* kiwerApi = new KiwerAPI();
+	Driver* nemoDriver = new MockDriver{ nemoApi };
+	Driver* kiwerDriver = new MockDriver{ nemoApi };
+	StockerBrockerInterface interface;
+
+	interface.setDriver("nemo", nemoDriver);
+	interface.setDriver("kiwer", kiwerDriver);
+
+	interface.selectStockBrocker("nemo");
+
+	interface.loginSystem("user", "pass");
+
+	std::ostringstream oss;
+	auto oldCoutStreamBuf = std::cout.rdbuf();
+	std::cout.rdbuf(oss.rdbuf());
+
+	std::cout.rdbuf(oldCoutStreamBuf);
+
+	string actual = oss.str();
+
+	EXPECT_EQ("[NEMO]user login GOOD\n", actual);
+}
+
+TEST(TradingSystem, KiwerLogin) {
+	NemoAPI* nemoApi = new NemoAPI();
+	KiwerAPI* kiwerApi = new KiwerAPI();
+	Driver* nemoDriver = new MockDriver{ nemoApi };
+	Driver* kiwerDriver = new MockDriver{ nemoApi };
+	StockerBrockerInterface interface;
+
+	interface.setDriver("nemo", nemoDriver);
+	interface.setDriver("kiwer", kiwerDriver);
+
+	interface.selectStockBrocker("kiwer");
+
+	interface.loginSystem("user", "pass");
+
+	std::ostringstream oss;
+	auto oldCoutStreamBuf = std::cout.rdbuf();
+	std::cout.rdbuf(oss.rdbuf());
+
+	std::cout.rdbuf(oldCoutStreamBuf);
+
+	string actual = oss.str();
+
+	EXPECT_EQ("user login success\n", actual);
+}
 
 int main(void) {
 	::testing::InitGoogleMock();
