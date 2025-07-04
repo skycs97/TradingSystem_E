1b019fe885196075857bbb19fc009a52ba070ded
soonwonchoi <soonwon.choi@samsung.com>
2025-07-04 12:45:38 +0900

    Add Feature - selectDriver()

--- TradingSystem/auto_trading_system.cpp
+++ TradingSystem/auto_trading_system.cpp
@@ -1,2 +1,23 @@
 #include "auto_trading_system.h"
 
+void AutoTradingSystem::addDriver(string name, Driver* driver)
+{
+	if (name == "nemo") nemo_driver = driver;
+	else if (name == "kiwer") kiwer_driver = driver;
+}
+
+void AutoTradingSystem::selectDriver(string name)
+{
+	if (name == "nemo") cur_driver = nemo_driver;
+	else if (name == "kiwer") cur_driver = kiwer_driver;
+}
+
+string AutoTradingSystem::getCurrentDriverName()
+{
+	assert(cur_driver != nullptr);
+
+	if (cur_driver == nemo_driver) return "nemo";
+	else if (cur_driver == kiwer_driver) return "kiwer";
+
+	throw std::exception();
+}
--- TradingSystem/auto_trading_system.h
+++ TradingSystem/auto_trading_system.h
@@ -1,6 +1,21 @@
 #pragma once
+#include "driver.h"
+#include <string>
+#include <stdexcept>
+#include <cassert>
+
+using std::string;
 
 class AutoTradingSystem {
 public:
+	void addDriver(string name, Driver* driver);
 
+	void selectDriver(string name);
+
+	string getCurrentDriverName();
+
+private:
+	Driver* nemo_driver{ nullptr };
+	Driver* kiwer_driver{ nullptr };
+	Driver* cur_driver{ nullptr };
 };
