fb449b56cf1d63615e64a678aeda501ed0952819
dongig.shin <dongig.shin@samsung.com>
2025-07-04 13:55:27 +0900

    update driver class (add header)

--- TradingSystem/kiwerDriver.cpp
+++ TradingSystem/kiwerDriver.cpp
@@ -1,28 +1,22 @@
-#include "driver.h"
-#include "kiwer.cpp"
+#include "kiwer_driver.h"
 #include <windows.h>
 
 using std::string;
 
-class kiwerDriver : public Driver {
-public:
-	void loginSystem(string stockCode, string pass) override
-	{
-		api.login(stockCode, pass);
-	}
-	void buyStock(string stockCode, int price, int count) override
-	{
-		api.buy(stockCode, price, count);
-	}
-	void sellStock(string stockCode, int price, int count) override
-	{
-		api.sell(stockCode, price, count);
-	}
-	int getMarketPrice(string stockCode, int after) override
-	{
-		Sleep(after);
-		return api.currentPrice(stockCode);
-	}
-private:
-	KiwerAPI api;
-};
+void kiwerDriver::loginSystem(string stockCode, string pass)
+{
+	api.login(stockCode, pass);
+}
+void kiwerDriver::buyStock(string stockCode, int price, int count)
+{
+	api.buy(stockCode, price, count);
+}
+void kiwerDriver::sellStock(string stockCode, int price, int count)
+{
+	api.sell(stockCode, price, count);
+}
+int kiwerDriver::getMarketPrice(string stockCode, int after)
+{
+	Sleep(after);
+	return api.currentPrice(stockCode);
+}
--- TradingSystem/nemoDriver.cpp
+++ TradingSystem/nemoDriver.cpp
@@ -1,27 +1,22 @@
-#include "driver.h"
-#include "nemo_api.cpp"
+#include "nemo_driver.h"
 
 using std::string;
 
-class nemoDriver : public Driver {
-public:
-	void loginSystem(string stockCode, string pass) override
-	{
-		api.certification(stockCode, pass);
-	}
-	void buyStock(string stockCode, int price, int count) override
-	{
-		api.purchasingStock(stockCode, price, count);
-	}
-	void sellStock(string stockCode, int price, int count) override
-	{
-		api.sellingStock(stockCode, price, count);
-	}
-	int getMarketPrice(string stockCode, int after) override
-	{
-		api.getMarketPrice(stockCode, after);
-		return 0;
-	}
-private:
-	NemoAPI api;
-};
+void nemoDriver::loginSystem(string stockCode, string pass)
+{
+	api.certification(stockCode, pass);
+}
+void nemoDriver::buyStock(string stockCode, int price, int count)
+{
+	api.purchasingStock(stockCode, price, count);
+}
+void nemoDriver::sellStock(string stockCode, int price, int count)
+{
+	api.sellingStock(stockCode, price, count);
+}
+int nemoDriver::getMarketPrice(string stockCode, int after)
+{
+	api.getMarketPrice(stockCode, after);
+	return 0;
+}
+
