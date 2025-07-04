61d44a2e71ed39c9677c8089f0a47b4060474acb
skycs97 <skycs97@gmail.com>
2025-07-04 15:55:14 +0900

    [UT] minus testcase bug fix

--- TradingSystem/test_AutoTradingSystem_with_mock.cpp
+++ TradingSystem/test_AutoTradingSystem_with_mock.cpp
@@ -105,11 +105,11 @@ TEST_F(AutoTradingSystemFixtureWithMock, BuyStockMinusPrice) {
 	EXPECT_THROW(system.buy("samsung",  - 1, 10), std::exception);
 }
 TEST_F(AutoTradingSystemFixtureWithMock, SellStockMinusPrice) {
-	EXPECT_THROW(system.sell("samsung", 10000, -1), std::exception);
+	EXPECT_THROW(system.sell("samsung", -1, 10000), std::exception);
 }
 
 TEST_F(AutoTradingSystemFixtureWithMock, BuyStockMinusCount) {
-	EXPECT_THROW(system.buy("samsung", -1, 10), std::exception);
+	EXPECT_THROW(system.buy("samsung", 10000, -1), std::exception);
 }
 TEST_F(AutoTradingSystemFixtureWithMock, SellStockMinusCount) {
 	EXPECT_THROW(system.sell("samsung", 10000, -1), std::exception);
